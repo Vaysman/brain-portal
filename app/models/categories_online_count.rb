7bb6b5a4f4590ebab4d049cbc837e70ae7fb84a3
Kiril Dokh <k.dokh@bvblogic.com>
2014-09-20 21:15:32 +0300

    fixes

--- app/models/categories_online_count.rb
+++ app/models/categories_online_count.rb
@@ -1,6 +1,10 @@
 class CategoriesOnlineCount < ActiveRecord::Base
   def self.get_count(id)
   	@record = self.find_by(:category_id => id)
-  	@record.requests
+  	if (@record)
+  	  @record.requests
+  	else
+  		0
+  	end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140920134223) do
+ActiveRecord::Schema.define(version: 20140920180544) do
 
   create_table "categories", force: true do |t|
     t.string   "title"
@@ -39,8 +39,8 @@ ActiveRecord::Schema.define(version: 20140920134223) do
   create_table "game_requests", force: true do |t|
     t.integer  "category_id"
     t.integer  "user_id"
-    t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "created_at"
   end
 
   add_index "game_requests", ["category_id"], name: "index_game_requests_on_category_id", using: :btree
